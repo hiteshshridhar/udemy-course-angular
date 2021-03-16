c58efa57c5692635e4b5571197fb9c75643d675f
Hitesh Shridhar <56676250+hiteshshridhar@users.noreply.github.com>
2021-03-16 10:34:34 +0100

    form properly rendered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udemy-course-angular';
+
+  onSubmit(formName: any): void {
+    console.log(formName);
+  }
 }
